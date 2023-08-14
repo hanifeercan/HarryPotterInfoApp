b2e7db1b8856c00426f16f617cceccd6c8c22e1e
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-14 15:23:02 +0300

    Add SplashScreenFragment in nav_graph

---
+++ app/src/main/java/com/hercan/harrypotterinfoapp/ui/splash/SplashScreenFragment.kt
@@ -0,0 +1,6 @@
+package com.hercan.harrypotterinfoapp.ui.splash
+
+import androidx.fragment.app.Fragment
+import com.hercan.harrypotterinfoapp.R
+
+class SplashScreenFragment : Fragment(R.layout.fragment_splash_screen)
