eaffc78b5774b0a7d2e234980893ee8b058fc787
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-15 15:53:06 +0300

    Add action to SplashScreenFragment

--- app/src/main/java/com/hercan/harrypotterinfoapp/ui/splash/SplashScreenFragment.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/ui/splash/SplashScreenFragment.kt
@@ -1,6 +1,20 @@
 package com.hercan.harrypotterinfoapp.ui.splash
 
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.hercan.harrypotterinfoapp.R
 
-class SplashScreenFragment : Fragment(R.layout.fragment_splash_screen)
+class SplashScreenFragment : Fragment(R.layout.fragment_splash_screen) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(SplashScreenFragmentDirections.navigateToHomeFragment())
+        }, 5000)
+    }
+}
