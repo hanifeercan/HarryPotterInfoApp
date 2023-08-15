792c3155de4ab933e15ceda24aa6dc6f096de5c0
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-15 15:41:57 +0300

    Create HomeFragment

---
+++ app/src/main/java/com/hercan/harrypotterinfoapp/ui/home/HomeFragment.kt
@@ -0,0 +1,6 @@
+package com.hercan.harrypotterinfoapp.ui.home
+
+import androidx.fragment.app.Fragment
+import com.hercan.harrypotterinfoapp.R
+
+class HomeFragment : Fragment(R.layout.fragment_home)
