7679c071496aa81a0743d5387c49856b8c8af7be
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-15 19:53:23 +0300

    Add AndroidEntryPoint implementation to MainActivity and HomeFragment

--- app/src/main/java/com/hercan/harrypotterinfoapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/ui/home/HomeFragment.kt
@@ -2,5 +2,7 @@ package com.hercan.harrypotterinfoapp.ui.home
 
 import androidx.fragment.app.Fragment
 import com.hercan.harrypotterinfoapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home)
