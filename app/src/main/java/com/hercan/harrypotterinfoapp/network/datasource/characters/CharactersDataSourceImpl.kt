d501fb4bc01e17fc5bfba640daa591eb4502adad
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-15 16:14:58 +0300

    Create CharactersDataSourceImpl

---
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/datasource/characters/CharactersDataSourceImpl.kt
@@ -0,0 +1,14 @@
+package com.hercan.harrypotterinfoapp.network.datasource.characters
+
+import com.hercan.harrypotterinfoapp.network.api.CharactersApi
+import com.hercan.harrypotterinfoapp.network.model.CharacterModel
+import retrofit2.Response
+import javax.inject.Inject
+
+class CharactersDataSourceImpl @Inject constructor(private val api: CharactersApi) :
+    CharactersDataSource {
+
+    override suspend fun getAllCharacters(): Response<List<CharacterModel>> {
+        return api.getAllCharacters()
+    }
+}
