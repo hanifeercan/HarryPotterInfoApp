3449fcb1b6aa33f67efa544bf8e19dc3b50efaa6
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-16 16:04:15 +0300

    Create PotterDBRepository interface

---
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/potterdb/PotterDBRepository.kt
@@ -0,0 +1,9 @@
+package com.hercan.harrypotterinfoapp.network.repository.potterdb
+
+import com.hercan.harrypotterinfoapp.network.model.PotionModel
+import com.hercan.harrypotterinfoapp.network.utils.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface PotterDBRepository {
+    suspend fun getAllPotions(): Flow<Resource<PotionModel>>
+}
