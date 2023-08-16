8ba75aee17143544a0520c7b446ace432d8e0160
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-16 16:03:32 +0300

    Create PotterDBDataSourceImpl

---
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/datasource/potterdb/PotterDBDataSourceImpl.kt
@@ -0,0 +1,14 @@
+package com.hercan.harrypotterinfoapp.network.datasource.potterdb
+
+import com.hercan.harrypotterinfoapp.network.api.PotterDBApi
+import com.hercan.harrypotterinfoapp.network.model.PotionModel
+import retrofit2.Response
+import javax.inject.Inject
+
+class PotterDBDataSourceImpl @Inject constructor(private val api: PotterDBApi) :
+    PotterDBDataSource {
+
+    override suspend fun getAllPotions(): Response<PotionModel> {
+        return api.getAllPotions()
+    }
+}
