7752e0cd4c9a0bb8562dfdcdbc045ca05fe396ba
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-16 17:32:45 +0300

    Create CharacterUIModel, Add strings and colors for CharacterUIModel

---
+++ app/src/main/java/com/hercan/presentation/model/CharacterUIModel.kt
@@ -0,0 +1,133 @@
+package com.hercan.presentation.model
+
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.hercan.harrypotterinfoapp.R
+import com.hercan.harrypotterinfoapp.network.model.character.CharacterModel
+
+data class CharacterUIModel(
+    val id: String?,
+    val actorName: String?,
+    @DrawableRes
+    val aliveId: Int,
+    val alternateNames: String?,
+    @StringRes
+    val typeId: Int,
+    val species: String?,
+    val gender: String?,
+    @StringRes
+    val hogwartsStaffOrStudentId: Int,
+    @DrawableRes
+    val houseDrawableId: Int,
+    @ColorRes
+    val houseColorId: Int,
+    val image: String?,
+    val characterName: String?,
+    val patronus: String?,
+    val wandCore: String?,
+)
+
+fun CharacterModel.toCharacterUIModel(): CharacterUIModel {
+    return this.let {
+        val id = it.id
+        val actorName = it.actor
+        val aliveId = if (it.alive == true) {
+            R.drawable.ic_live
+        } else {
+            R.drawable.ic_dead
+        }
+        val alternateNames = it.alternateNames.toAlternativeNames()
+
+        val typeId = if (it.wizard == true) {
+            R.string.wizard
+        } else {
+            if (it.ancestry == "squib") {
+                R.string.squib
+            } else if (it.species == "human") {
+                R.string.muggle
+            } else {
+                R.string.animal
+            }
+        }
+        val species = it.species
+        val gender = it.gender
+
+        val hogwartsStaffOrStudentId = if (it.hogwartsStudent == true) {
+            R.string.hogwarts_student
+        } else if (it.hogwartsStaff == true) {
+            R.string.hogwarts_stuff
+        } else {
+            R.string.not_a_student_or_stuff
+        }
+
+        val houseDrawableId = when (it.house) {
+            "Gryffindor" -> {
+                R.drawable.ic_gryffindor
+            }
+            "Slytherin" -> {
+                R.drawable.ic_slytherin
+            }
+            "Hufflepuff" -> {
+                R.drawable.ic_hufflepuff
+            }
+            "Ravenclaw" -> {
+                R.drawable.ic_ravenclaw
+            }
+            else -> {
+                R.drawable.ic_hp
+            }
+        }
+
+        val houseColorId = when (it.house) {
+            "Gryffindor" -> {
+                R.color.gryffindor_red
+            }
+            "Slytherin" -> {
+                R.color.slytherin_green
+            }
+            "Hufflepuff" -> {
+                R.color.hufflepuff_yellow
+            }
+            "Ravenclaw" -> {
+                R.color.ravenclaw_blue
+            }
+            else -> {
+                R.color.bg_main
+            }
+        }
+
+        val image = if (it.image?.isEmpty() == true) null else it.image
+        val characterName = it.name
+        val patronus = it.patronus
+        val wandCore = it.wand?.core
+
+        CharacterUIModel(
+            id,
+            actorName,
+            aliveId,
+            alternateNames,
+            typeId,
+            species,
+            gender,
+            hogwartsStaffOrStudentId,
+            houseDrawableId,
+            houseColorId,
+            image,
+            characterName,
+            patronus,
+            wandCore,
+        )
+    }
+}
+
+fun List<String?>?.toAlternativeNames(): String {
+    var str = ""
+    this?.forEachIndexed { index, s ->
+        str += s
+        if (index != (this.size - 1)) {
+            str += "\n\t\t\t\t"
+        }
+    }
+    return str
+}
