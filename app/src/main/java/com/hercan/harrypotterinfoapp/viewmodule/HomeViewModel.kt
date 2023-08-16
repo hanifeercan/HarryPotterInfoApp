aceb9f20fd61e2f3697e00a6505445ed3214e4c3
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-16 16:13:45 +0300

    Add getPotions function in HomeViewModel

--- app/src/main/java/com/hercan/harrypotterinfoapp/viewmodule/HomeViewModel.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/viewmodule/HomeViewModel.kt
@@ -3,7 +3,8 @@ package com.hercan.harrypotterinfoapp.viewmodule
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.hercan.harrypotterinfoapp.network.repository.CharactersRepositoryImpl
+import com.hercan.harrypotterinfoapp.network.repository.characters.CharactersRepositoryImpl
+import com.hercan.harrypotterinfoapp.network.repository.potterdb.PotterDBRepositoryImpl
 import com.hercan.harrypotterinfoapp.network.utils.Status
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -13,11 +14,15 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(private val repository: CharactersRepositoryImpl) :
+class HomeViewModel @Inject constructor(
+    private val repository: CharactersRepositoryImpl,
+    private val potterRepository: PotterDBRepositoryImpl
+) :
     ViewModel() {
 
     init {
         getCharacters()
+        getPotions()
     }
 
     fun getCharacters() {
@@ -34,6 +39,25 @@ class HomeViewModel @Inject constructor(private val repository: CharactersReposi
                         Log.d("charactersData", it.data.toString())
                     } else {
                         Log.e("charactersData", it.message.toString())
+                    }
+                }
+        }
+    }
+
+    fun getPotions() {
+        viewModelScope.launch(Dispatchers.IO) {
+            potterRepository.getAllPotions()
+                .onStart {
+                    Log.d("potionsData", "onStart")
+                }
+                .onCompletion {
+                    Log.d("potionsData", "onCompletion")
+                }
+                .collect {
+                    if (it.status == Status.SUCCESS) {
+                        Log.d("potionsData", it.data.toString())
+                    } else {
+                        Log.e("potionsData", it.message.toString())
                     }
                 }
         }
