d365094620b5e17fee052595cd92b6dee40cba6f
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-16 16:28:39 +0300

    Add getAllSpells suspend function in PotterDBDataSource interface

--- app/src/main/java/com/hercan/harrypotterinfoapp/network/datasource/potterdb/PotterDBDataSource.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/datasource/potterdb/PotterDBDataSource.kt
@@ -1,8 +1,10 @@
 package com.hercan.harrypotterinfoapp.network.datasource.potterdb
 
 import com.hercan.harrypotterinfoapp.network.model.potion.PotionModel
+import com.hercan.harrypotterinfoapp.network.model.spell.SpellModel
 import retrofit2.Response
 
 interface PotterDBDataSource {
     suspend fun getAllPotions(): Response<PotionModel>
+    suspend fun getAllSpells(): Response<SpellModel>
 }
