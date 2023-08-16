12c555dbc8958a48325610340afc2c454ec8d521
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-16 16:28:05 +0300

    Add getAllSpells suspend function

--- app/src/main/java/com/hercan/harrypotterinfoapp/network/api/PotterDBApi.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/api/PotterDBApi.kt
@@ -1,12 +1,15 @@
 package com.hercan.harrypotterinfoapp.network.api
 
 import com.hercan.harrypotterinfoapp.network.model.potion.PotionModel
+import com.hercan.harrypotterinfoapp.network.model.spell.SpellModel
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface PotterDBApi {
 
     @GET("potions")
     suspend fun getAllPotions(): Response<PotionModel>
+
+    @GET("spells")
+    suspend fun getAllSpells(): Response<SpellModel>
 }
