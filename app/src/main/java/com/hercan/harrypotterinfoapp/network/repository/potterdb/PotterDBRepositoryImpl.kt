18b30c70dccbcfdcdf86282db9014b84adcf127e
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-16 16:30:52 +0300

    Override getAllSpells suspend function in PotterDBRepositoryImpl

--- app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/potterdb/PotterDBRepositoryImpl.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/potterdb/PotterDBRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.hercan.harrypotterinfoapp.network.repository.potterdb
 
 import com.hercan.harrypotterinfoapp.network.datasource.potterdb.PotterDBDataSource
 import com.hercan.harrypotterinfoapp.network.model.potion.PotionModel
+import com.hercan.harrypotterinfoapp.network.model.spell.SpellModel
 import com.hercan.harrypotterinfoapp.network.utils.NetworkExt
 import com.hercan.harrypotterinfoapp.network.utils.Resource
 import kotlinx.coroutines.flow.Flow
@@ -15,6 +16,12 @@ class PotterDBRepositoryImpl @Inject constructor(
     override suspend fun getAllPotions(): Flow<Resource<PotionModel>> {
         return flow {
             emit(safeApiCall { dataSource.getAllPotions() })
+        }
+    }
+
+    override suspend fun getAllSpells(): Flow<Resource<SpellModel>> {
+        return flow {
+            emit(safeApiCall { dataSource.getAllSpells() })
         }
     }
 }
