a336ae0cc34049e45631178eb6997eaf8464e2b2
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-16 16:30:26 +0300

    Add getAllSpells suspend function in PotterDBRepository interface

--- app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/potterdb/PotterDBRepository.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/potterdb/PotterDBRepository.kt
@@ -1,9 +1,11 @@
 package com.hercan.harrypotterinfoapp.network.repository.potterdb
 
 import com.hercan.harrypotterinfoapp.network.model.potion.PotionModel
+import com.hercan.harrypotterinfoapp.network.model.spell.SpellModel
 import com.hercan.harrypotterinfoapp.network.utils.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface PotterDBRepository {
     suspend fun getAllPotions(): Flow<Resource<PotionModel>>
+    suspend fun getAllSpells(): Flow<Resource<SpellModel>>
 }
