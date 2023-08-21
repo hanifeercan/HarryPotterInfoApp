e75408018621e4be11c762b19c7843092d07a1dd
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-21 16:45:01 +0300

    Clean import

--- app/src/main/java/com/hercan/harrypotterinfoapp/network/model/potion/PotionModel.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/model/potion/PotionModel.kt
@@ -1,7 +1,6 @@
 package com.hercan.harrypotterinfoapp.network.model.potion
 
 import com.google.gson.annotations.SerializedName
-import com.hercan.harrypotterinfoapp.network.model.potion.PotionData
 
 data class PotionModel(
     @SerializedName("data")
