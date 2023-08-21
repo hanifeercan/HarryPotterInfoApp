5f7c5deb572af5ad85506027d78c9d498afd90af
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-21 20:17:57 +0300

    Create SpellDetailFragment

---
+++ app/src/main/java/com/hercan/harrypotterinfoapp/ui/detail/SpellDetailFragment.kt
@@ -0,0 +1,6 @@
+package com.hercan.harrypotterinfoapp.ui.detail
+
+import androidx.fragment.app.Fragment
+import com.hercan.harrypotterinfoapp.R
+
+class SpellDetailFragment : Fragment(R.layout.fragment_spell_detail)
