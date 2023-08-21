bb9f166cb2037a962bfe945e9eb6e8323a74c77b
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-21 18:59:38 +0300

    Add itemClickListener in SpellsAdapter

--- app/src/main/java/com/hercan/harrypotterinfoapp/presentation/adapter/SpellsAdapter.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/presentation/adapter/SpellsAdapter.kt
@@ -13,7 +13,9 @@ import com.squareup.picasso.Picasso
 class SpellsAdapter :
     ListAdapter<SpellUIModel, SpellsAdapter.ViewHolder>(DiffCallback) {
 
-    class ViewHolder(
+    private var itemClickListener: ((SpellUIModel) -> Unit)? = null
+
+    inner class ViewHolder(
         private val binding: ItemPotterdbBinding,
     ) :
         RecyclerView.ViewHolder(binding.root) {
@@ -28,7 +30,14 @@ class SpellsAdapter :
                 .placeholder(R.drawable.iv_default_spell_photo)
                 .into(ivPhoto)
 
+            root.setOnClickListener {
+                itemClickListener?.invoke(item)
+            }
         }
+    }
+
+    fun setItemClickListener(listener: (SpellUIModel) -> Unit) {
+        this.itemClickListener = listener
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
