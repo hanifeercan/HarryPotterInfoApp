b7856add6d7b2c5d160bd69f27b06cd9b0924337
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-21 19:12:38 +0300

    Create PotionDetailFragment

---
+++ app/src/main/java/com/hercan/harrypotterinfoapp/ui/detail/PotionDetailFragment.kt
@@ -0,0 +1,6 @@
+package com.hercan.harrypotterinfoapp.ui.detail
+
+import androidx.fragment.app.Fragment
+import com.hercan.harrypotterinfoapp.R
+
+class PotionDetailFragment : Fragment(R.layout.fragment_potion_detail)
