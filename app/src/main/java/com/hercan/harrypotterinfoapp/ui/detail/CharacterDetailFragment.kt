63c3b95784b0f485b37aa78a0b8dc2fa8791887a
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-21 19:01:55 +0300

    Create CharacterDetailFragment

---
+++ app/src/main/java/com/hercan/harrypotterinfoapp/ui/detail/CharacterDetailFragment.kt
@@ -0,0 +1,6 @@
+package com.hercan.harrypotterinfoapp.ui.detail
+
+import androidx.fragment.app.Fragment
+import com.hercan.harrypotterinfoapp.R
+
+class CharacterDetailFragment : Fragment(R.layout.fragment_character_detail)
