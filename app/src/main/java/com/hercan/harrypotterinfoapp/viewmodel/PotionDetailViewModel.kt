53dcd0aede9b91fa8fc75458c3ce86f60a56bc66
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-22 12:42:44 +0300

    Delete unused import

--- app/src/main/java/com/hercan/harrypotterinfoapp/viewmodel/PotionDetailViewModel.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/viewmodel/PotionDetailViewModel.kt
@@ -1,6 +1,5 @@
 package com.hercan.harrypotterinfoapp.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
