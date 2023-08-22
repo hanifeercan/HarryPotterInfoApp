bd1ba85ee3f89a5bc03ab8ece8ba947dd5b2bcd5
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-22 14:47:08 +0300

    Add getSpellWithSlug suspend function in PotterDBDataSource

--- app/src/main/java/com/hercan/harrypotterinfoapp/network/datasource/potterdb/PotterDBDataSource.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/datasource/potterdb/PotterDBDataSource.kt
@@ -2,6 +2,7 @@ package com.hercan.harrypotterinfoapp.network.datasource.potterdb
 
 import com.hercan.harrypotterinfoapp.network.model.potion.PotionDetailModel
 import com.hercan.harrypotterinfoapp.network.model.potion.PotionModel
+import com.hercan.harrypotterinfoapp.network.model.spell.SpellDetailModel
 import com.hercan.harrypotterinfoapp.network.model.spell.SpellModel
 import retrofit2.Response
 
@@ -9,4 +10,5 @@ interface PotterDBDataSource {
     suspend fun getAllPotions(): Response<PotionModel>
     suspend fun getAllSpells(): Response<SpellModel>
     suspend fun getPotionWithSlug(slug: String): Response<PotionDetailModel>
+    suspend fun getSpellWithSlug(slug: String): Response<SpellDetailModel>
 }
