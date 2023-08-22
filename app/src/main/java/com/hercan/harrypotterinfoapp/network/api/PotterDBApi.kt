e364ac2024c0e4ba84558a713cc5b70c9cf463d6
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-22 12:35:39 +0300

    Add getPotionWithSlug suspend function in PotterDBApi

--- app/src/main/java/com/hercan/harrypotterinfoapp/network/api/PotterDBApi.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/api/PotterDBApi.kt
@@ -1,9 +1,11 @@
 package com.hercan.harrypotterinfoapp.network.api
 
+import com.hercan.harrypotterinfoapp.network.model.potion.PotionDetailModel
 import com.hercan.harrypotterinfoapp.network.model.potion.PotionModel
 import com.hercan.harrypotterinfoapp.network.model.spell.SpellModel
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PotterDBApi {
 
@@ -12,4 +14,8 @@ interface PotterDBApi {
 
     @GET("spells")
     suspend fun getAllSpells(): Response<SpellModel>
+
+    @GET("potions/{slug}")
+    suspend fun getPotionWithSlug(@Path("slug") slug: String): Response<PotionDetailModel>
+
 }
