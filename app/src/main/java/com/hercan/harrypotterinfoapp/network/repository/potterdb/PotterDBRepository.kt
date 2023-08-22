a9df20eb65277fc1c9684aed5b485638f938925c
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-22 12:37:01 +0300

    Add getPotionWithSlug function in PotterDBRepository

--- app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/potterdb/PotterDBRepository.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/potterdb/PotterDBRepository.kt
@@ -1,5 +1,6 @@
 package com.hercan.harrypotterinfoapp.network.repository.potterdb
 
+import com.hercan.harrypotterinfoapp.network.model.potion.PotionDetailModel
 import com.hercan.harrypotterinfoapp.network.model.potion.PotionModel
 import com.hercan.harrypotterinfoapp.network.model.spell.SpellModel
 import com.hercan.harrypotterinfoapp.network.utils.Resource
@@ -8,4 +9,5 @@ import kotlinx.coroutines.flow.Flow
 interface PotterDBRepository {
     fun getAllPotions(): Flow<Resource<PotionModel>>
     fun getAllSpells(): Flow<Resource<SpellModel>>
+    fun getPotionWithSlug(slug: String): Flow<Resource<PotionDetailModel>>
 }
