610156b8af1c4c39c04df48a9a0879605351f6aa
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-23 21:48:58 +0300

    Set animationView in PotionsAdapter

--- app/src/main/java/com/hercan/harrypotterinfoapp/presentation/adapter/PotionsAdapter.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/presentation/adapter/PotionsAdapter.kt
@@ -8,9 +8,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.hercan.harrypotterinfoapp.R
 import com.hercan.harrypotterinfoapp.databinding.ItemPotterdbBinding
 import com.hercan.harrypotterinfoapp.presentation.model.PotionUIModel
+import com.hercan.harrypotterinfoapp.presentation.room.Favorite
+import com.hercan.harrypotterinfoapp.presentation.room.FavoriteDatabase
 import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class PotionsAdapter :
+class PotionsAdapter(private val db: FavoriteDatabase?) :
     ListAdapter<PotionUIModel, PotionsAdapter.ViewHolder>(DiffCallback) {
 
     private var itemClickListener: ((PotionUIModel) -> Unit)? = null
@@ -32,6 +37,31 @@ class PotionsAdapter :
 
             root.setOnClickListener {
                 itemClickListener?.invoke(item)
+            }
+
+            val dao = db?.favoriteDao()
+            val coroutineScope = CoroutineScope(Dispatchers.Default)
+            val type = root.context.getString(R.string.potion)
+            coroutineScope.launch {
+                val favoriteCharactersId = dao?.findId(item.id ?: "", type)
+                if (favoriteCharactersId == item.id) {
+                    animationView.progress = 1.0F
+                } else {
+                    animationView.progress = 0.0F
+                }
+            }
+            animationView.setOnClickListener {
+                if (animationView.progress == 0.0F) {
+                    animationView.playAnimation()
+                    coroutineScope.launch {
+                        dao?.insert(Favorite(item.id ?: "", type))
+                    }
+                } else {
+                    animationView.progress = 0.0F
+                    coroutineScope.launch {
+                        dao?.delete(Favorite(item.id ?: "", type))
+                    }
+                }
             }
         }
     }
