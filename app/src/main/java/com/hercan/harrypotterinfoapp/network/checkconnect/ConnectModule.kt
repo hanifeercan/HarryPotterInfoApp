3a9ab3c3189e3b8146e67f1e8c5902215d25a807
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-24 14:48:48 +0300

    Create ConnectModule

---
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/checkconnect/ConnectModule.kt
@@ -0,0 +1,22 @@
+package com.hercan.harrypotterinfoapp.network.checkconnect
+
+import android.content.Context
+import android.net.ConnectivityManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ConnectModule {
+
+    @Provides
+    @Singleton
+    fun provideConnectivityManager(@ApplicationContext context: Context): ConnectivityManager {
+        return context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    }
+
+}
