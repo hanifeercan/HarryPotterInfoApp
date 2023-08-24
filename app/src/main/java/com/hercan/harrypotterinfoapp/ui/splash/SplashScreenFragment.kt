dea8f972be54da115df0ccd18e15788e119864a0
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-24 10:02:14 +0300

    Edit for orientation in SplashScreenFragment

--- app/src/main/java/com/hercan/harrypotterinfoapp/ui/splash/SplashScreenFragment.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/ui/splash/SplashScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.hercan.harrypotterinfoapp.ui.splash
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -12,6 +13,8 @@ class SplashScreenFragment : Fragment(R.layout.fragment_splash_screen) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LOCKED
 
         Handler(Looper.getMainLooper()).postDelayed({
             findNavController().navigate(SplashScreenFragmentDirections.navigateToHomeFragment())
