ed84b86b844d2cf9cfa64e26c51d2adf686924c3
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-25 03:52:21 +0300

    Add favoriteCharacterDao functions the CharactersRepository

--- app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/characters/CharactersRepository.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/characters/CharactersRepository.kt
@@ -2,8 +2,11 @@ package com.hercan.harrypotterinfoapp.network.repository.characters
 
 import com.hercan.harrypotterinfoapp.network.model.character.CharacterModel
 import com.hercan.harrypotterinfoapp.network.utils.Resource
+import com.hercan.harrypotterinfoapp.presentation.room.model.FavoriteCharacterModel
 import kotlinx.coroutines.flow.Flow
 
 interface CharactersRepository {
     fun getAllCharacters(): Flow<Resource<List<CharacterModel>>>
+    suspend fun update(id: String, isFavorite: Boolean)
+    suspend fun insert(favoriteCharacterModel: FavoriteCharacterModel)
 }
--- app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/characters/CharactersRepositoryImpl.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/characters/CharactersRepositoryImpl.kt
@@ -1,14 +1,18 @@
 package com.hercan.harrypotterinfoapp.network.repository.characters
 
+import androidx.annotation.WorkerThread
 import com.hercan.harrypotterinfoapp.network.datasource.characters.CharactersDataSource
 import com.hercan.harrypotterinfoapp.network.model.character.CharacterModel
 import com.hercan.harrypotterinfoapp.network.utils.NetworkExt
 import com.hercan.harrypotterinfoapp.network.utils.Resource
+import com.hercan.harrypotterinfoapp.presentation.room.dao.FavoriteCharactersDao
+import com.hercan.harrypotterinfoapp.presentation.room.model.FavoriteCharacterModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class CharactersRepositoryImpl @Inject constructor(
+    private val favoriteCharactersDao: FavoriteCharactersDao,
     private val dataSource: CharactersDataSource,
 ) : NetworkExt(), CharactersRepository {
 
@@ -16,5 +20,26 @@ class CharactersRepositoryImpl @Inject constructor(
         return flow {
             emit(safeApiCall { dataSource.getAllCharacters() })
         }
+    }
+
+    var allFavoriteCharacters: Flow<List<FavoriteCharacterModel>> =
+        favoriteCharactersDao.getCharacters()
+
+    private fun getAllFavoriteCharacters() {
+        allFavoriteCharacters = favoriteCharactersDao.getCharacters()
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    override suspend fun update(id: String, isFavorite: Boolean) {
+        favoriteCharactersDao.update(id, isFavorite)
+        getAllFavoriteCharacters()
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    override suspend fun insert(favoriteCharacterModel: FavoriteCharacterModel) {
+        favoriteCharactersDao.insert(favoriteCharacterModel)
+        getAllCharacters()
     }
 }
