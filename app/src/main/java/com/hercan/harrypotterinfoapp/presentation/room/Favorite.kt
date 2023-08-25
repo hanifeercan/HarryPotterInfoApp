d075c251d7297f712df243516f02bd785d8420e4
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-25 03:45:11 +0300

    Delete room database files

--- app/src/main/java/com/hercan/harrypotterinfoapp/presentation/room/Favorite.kt
+++
@@ -1,7 +0,0 @@
-package com.hercan.harrypotterinfoapp.presentation.room
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "favorite")
-data class Favorite(@PrimaryKey val id: String, val type: String)
