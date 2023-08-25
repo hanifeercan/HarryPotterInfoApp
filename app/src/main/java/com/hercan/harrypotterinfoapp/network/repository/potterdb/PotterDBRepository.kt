afda49cb384e91a52077e33e0a8e676feb23c129
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-25 03:53:22 +0300

    Add FavoritePotionsDao and FavoriteSpellsDao functions the PotterDBRepository

--- app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/potterdb/PotterDBRepository.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/potterdb/PotterDBRepository.kt
@@ -5,6 +5,8 @@ import com.hercan.harrypotterinfoapp.network.model.potion.PotionModel
 import com.hercan.harrypotterinfoapp.network.model.spell.SpellDetailModel
 import com.hercan.harrypotterinfoapp.network.model.spell.SpellModel
 import com.hercan.harrypotterinfoapp.network.utils.Resource
+import com.hercan.harrypotterinfoapp.presentation.room.model.FavoritePotionModel
+import com.hercan.harrypotterinfoapp.presentation.room.model.FavoriteSpellModel
 import kotlinx.coroutines.flow.Flow
 
 interface PotterDBRepository {
@@ -12,4 +14,8 @@ interface PotterDBRepository {
     fun getAllSpells(): Flow<Resource<SpellModel>>
     fun getPotionWithSlug(slug: String): Flow<Resource<PotionDetailModel>>
     fun getSpellWithSlug(slug: String): Flow<Resource<SpellDetailModel>>
+    suspend fun updatePotion(id: String, isFavorite: Boolean)
+    suspend fun insertPotion(favoritePotionModel: FavoritePotionModel)
+    suspend fun updateSpell(id: String, isFavorite: Boolean)
+    suspend fun insertSpell(favoriteSpellModel: FavoriteSpellModel)
 }
--- app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/potterdb/PotterDBRepositoryImpl.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/repository/potterdb/PotterDBRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.hercan.harrypotterinfoapp.network.repository.potterdb
 
+import androidx.annotation.WorkerThread
 import com.hercan.harrypotterinfoapp.network.datasource.potterdb.PotterDBDataSource
 import com.hercan.harrypotterinfoapp.network.model.potion.PotionDetailModel
 import com.hercan.harrypotterinfoapp.network.model.potion.PotionModel
@@ -7,11 +8,17 @@ import com.hercan.harrypotterinfoapp.network.model.spell.SpellDetailModel
 import com.hercan.harrypotterinfoapp.network.model.spell.SpellModel
 import com.hercan.harrypotterinfoapp.network.utils.NetworkExt
 import com.hercan.harrypotterinfoapp.network.utils.Resource
+import com.hercan.harrypotterinfoapp.presentation.room.dao.FavoritePotionsDao
+import com.hercan.harrypotterinfoapp.presentation.room.dao.FavoriteSpellsDao
+import com.hercan.harrypotterinfoapp.presentation.room.model.FavoritePotionModel
+import com.hercan.harrypotterinfoapp.presentation.room.model.FavoriteSpellModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class PotterDBRepositoryImpl @Inject constructor(
+    private val favoritePotionsDao: FavoritePotionsDao,
+    private val favoriteSpellsDao: FavoriteSpellsDao,
     private val dataSource: PotterDBDataSource,
 ) : NetworkExt(), PotterDBRepository {
 
@@ -37,5 +44,47 @@ class PotterDBRepositoryImpl @Inject constructor(
         return flow {
             emit(safeApiCall { dataSource.getSpellWithSlug(slug) })
         }
+    }
+
+    var allFavoritePotions: Flow<List<FavoritePotionModel>> =
+        favoritePotionsDao.getPotions()
+
+    private fun getAllFavoritePotions() {
+        allFavoritePotions = favoritePotionsDao.getPotions()
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    override suspend fun updatePotion(id: String, isFavorite: Boolean) {
+        favoritePotionsDao.updatePotion(id, isFavorite)
+        getAllFavoritePotions()
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    override suspend fun insertPotion(favoritePotionModel: FavoritePotionModel) {
+        favoritePotionsDao.insertPotion(favoritePotionModel)
+        getAllFavoritePotions()
+    }
+
+    var allFavoriteSpells: Flow<List<FavoriteSpellModel>> =
+        favoriteSpellsDao.getSpells()
+
+    private fun getAllFavoriteSpells() {
+        allFavoriteSpells = favoriteSpellsDao.getSpells()
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    override suspend fun updateSpell(id: String, isFavorite: Boolean) {
+        favoriteSpellsDao.updateSpell(id, isFavorite)
+        getAllFavoriteSpells()
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    override suspend fun insertSpell(favoriteSpellModel: FavoriteSpellModel) {
+        favoriteSpellsDao.insertSpell(favoriteSpellModel)
+        getAllFavoriteSpells()
     }
 }
