961be940a988902c2f76b6c6e9462cde70a2a405
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-25 03:54:02 +0300

    Edits Repository Modules

--- app/src/main/java/com/hercan/harrypotterinfoapp/network/di/repositorymodule/PotterDBRepositoryModule.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/network/di/repositorymodule/PotterDBRepositoryModule.kt
@@ -3,6 +3,9 @@ package com.hercan.harrypotterinfoapp.network.di.repositorymodule
 import com.hercan.harrypotterinfoapp.network.datasource.potterdb.PotterDBDataSourceImpl
 import com.hercan.harrypotterinfoapp.network.repository.potterdb.PotterDBRepository
 import com.hercan.harrypotterinfoapp.network.repository.potterdb.PotterDBRepositoryImpl
+import com.hercan.harrypotterinfoapp.presentation.room.dao.FavoritePotionsDao
+import com.hercan.harrypotterinfoapp.presentation.room.dao.FavoriteSpellsDao
+import com.hercan.harrypotterinfoapp.presentation.room.database.FavoriteDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,7 +18,21 @@ object PotterDBRepositoryModule {
 
     @Provides
     @Singleton
-    fun providePotterDBRepository(dataSource: PotterDBDataSourceImpl): PotterDBRepository {
-        return PotterDBRepositoryImpl(dataSource)
+    fun providePotterDBRepository(
+        favoritePotionsDao: FavoritePotionsDao,
+        favoriteSpellsDao: FavoriteSpellsDao,
+        dataSource: PotterDBDataSourceImpl
+    ): PotterDBRepository {
+        return PotterDBRepositoryImpl(favoritePotionsDao, favoriteSpellsDao, dataSource)
+    }
+
+    @Provides
+    fun provideFavoritePotionsDao(favoriteDatabase: FavoriteDatabase): FavoritePotionsDao {
+        return favoriteDatabase.favoritePotionsDao()
+    }
+
+    @Provides
+    fun provideFavoriteSpellsDao(favoriteDatabase: FavoriteDatabase): FavoriteSpellsDao {
+        return favoriteDatabase.favoriteSpellsDao()
     }
 }
