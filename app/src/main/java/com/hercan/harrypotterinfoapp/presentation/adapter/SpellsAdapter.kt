e6d120f1250fdf85882b09d40834815ba643d7f5
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2023-08-25 03:58:07 +0300

    Edits for favoriteSpell in SpellsAdapter

--- app/src/main/java/com/hercan/harrypotterinfoapp/presentation/adapter/SpellsAdapter.kt
+++ app/src/main/java/com/hercan/harrypotterinfoapp/presentation/adapter/SpellsAdapter.kt
@@ -8,17 +8,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.hercan.harrypotterinfoapp.R
 import com.hercan.harrypotterinfoapp.databinding.ItemPotterdbBinding
 import com.hercan.harrypotterinfoapp.presentation.model.SpellUIModel
-import com.hercan.harrypotterinfoapp.presentation.room.Favorite
-import com.hercan.harrypotterinfoapp.presentation.room.FavoriteDatabase
+import com.hercan.harrypotterinfoapp.presentation.room.model.FavoriteSpellModel
 import com.squareup.picasso.Picasso
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
-class SpellsAdapter(private val db: FavoriteDatabase?) :
+class SpellsAdapter :
     ListAdapter<SpellUIModel, SpellsAdapter.ViewHolder>(DiffCallback) {
 
     private var itemClickListener: ((SpellUIModel) -> Unit)? = null
+    private var updateFavorite: ((FavoriteSpellModel, Boolean) -> Unit)? = null
+    var favoriteSpells: List<FavoriteSpellModel> = listOf()
 
     inner class ViewHolder(
         private val binding: ItemPotterdbBinding,
@@ -26,7 +24,6 @@ class SpellsAdapter(private val db: FavoriteDatabase?) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: SpellUIModel) = with(binding) {
 
-            setIsRecyclable(false)
             tvName.text = item.name
 
             Picasso.get()
@@ -38,33 +35,38 @@ class SpellsAdapter(private val db: FavoriteDatabase?) :
             root.setOnClickListener {
                 itemClickListener?.invoke(item)
             }
-            val dao = db?.favoriteDao()
-            val coroutineScope = CoroutineScope(Dispatchers.Default)
-            val type = root.context.getString(R.string.spell)
 
-            coroutineScope.launch {
-                val favoriteCharactersId = dao?.findId(item.id ?: "", type)
-                if (favoriteCharactersId == item.id) {
-                    animationView.progress = 1.0F
-                } else {
-                    animationView.progress = 0.0F
+            if (favoriteSpells.isNotEmpty()) {
+                val favorite = favoriteSpells.find {
+                    it.id == item.id
+                }
+                if (favorite != null) {
+                    if (favorite.isFavorite) {
+                        animationView.progress = 1.0F
+                    }
                 }
             }
 
             animationView.setOnClickListener {
-                if (animationView.progress == 0.0F) {
-                    animationView.playAnimation()
-                    coroutineScope.launch {
-                        dao?.insert(Favorite(item.id ?: "", type))
-                    }
-                } else {
-                    animationView.progress = 0.0F
-                    coroutineScope.launch {
-                        dao?.delete(Favorite(item.id ?: "", type))
+                val favorite = favoriteSpells.find {
+                    it.id == item.id
+                }
+                if (favorite != null) {
+                    if (favorite.isFavorite) {
+                        animationView.cancelAnimation()
+                        animationView.progress = 0.0F
+                        updateFavorite?.invoke(favorite, false)
+                    } else {
+                        updateFavorite?.invoke(favorite, true)
+                        animationView.playAnimation()
                     }
                 }
             }
         }
+    }
+
+    fun updateFavorite(updateFavorite: (FavoriteSpellModel, Boolean) -> Unit) {
+        this.updateFavorite = updateFavorite
     }
 
     fun setItemClickListener(listener: (SpellUIModel) -> Unit) {
